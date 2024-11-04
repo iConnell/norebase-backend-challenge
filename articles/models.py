5b0a43607289ba155c177ac35cc962e8a64b6409
Ikechukwu Ugwuanyi <connell@McConnell.local>
2024-11-04 22:51:48 +0100

    chore: add like_article and get_like_count views

--- articles/models.py
+++ articles/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
 # Create your models here.
+
+from django.db import models
+
+
+# Simple Article Model
+class Article(models.Model):
+    title = models.CharField(max_length=200)
+    content = models.TextField()
+
+
+# Simple Like model
+class Like(models.Model):
+    article = models.OneToOneField(
+        Article, on_delete=models.CASCADE, related_name="like"
+    )
+    count = models.IntegerField(default=0)
+
+    # user - this would have been here if auth was implemented
--- articles/views.py
+++ articles/views.py
@@ -1,3 +1,44 @@
 from django.shortcuts import render
+from django.shortcuts import get_object_or_404
+from .models import Article, Like
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
+from rest_framework import status
 
 # Create your views here.
+
+
+@api_view(http_method_names=["GET"])
+def get_like_count(request, article_id):
+    article = Article.objects.filter(pk=article_id).first()
+    if not article:
+        return Response(
+            {"status": "error", "message": "Article not found"},
+            status=status.HTTP_404_NOT_FOUND,
+        )
+
+    like, created = Like.objects.get_or_create(article=article)
+
+    return Response(
+        {"status": "success", "data": {"like_count": like.count}},
+        status=status.HTTP_200_OK,
+    )
+
+
+def like_article(request, article_id):
+    article = Article.objects.filter(pk=article_id).first()
+    if not article:
+        return Response(
+            {"status": "error", "message": "Article not found"},
+            status=status.HTTP_404_NOT_FOUND,
+        )
+
+    like, created = Like.objects.get_or_create(article=article)
+
+    like.count += 1
+    like.save()
+
+    return Response(
+        {"status": "success", "data": {"like_count": like.count}},
+        status=status.HTTP_200_OK,
+    )
