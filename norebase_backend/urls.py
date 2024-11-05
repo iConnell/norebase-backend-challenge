62bf846b842b8732184e520a66d00287448b8c05
Ikechukwu Ugwuanyi <connell@McConnell.local>
2024-11-05 08:37:28 +0100

    feat: add home view to list available routes

--- norebase_backend/urls.py
+++ norebase_backend/urls.py
@@ -17,5 +17,10 @@ Including another URLconf
 
 from django.contrib import admin
 from django.urls import path, include
+from articles.views import home
 
-urlpatterns = [path("admin/", admin.site.urls), path("api/", include("articles.urls"))]
+urlpatterns = [
+    # path("admin/", admin.site.urls),
+    path("", home, name="home"),
+    path("api/", include("articles.urls")),
+]
